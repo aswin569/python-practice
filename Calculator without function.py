46cada2b594f2909afb1beda69d55453b42d0366
aswin569 <99206624+aswin569@users.noreply.github.com>
2024-07-19 22:52:23 +0530

    Add files via upload

---
+++ Calculator without function.py
@@ -0,0 +1,24 @@
+num1 = int(input("Enter the first number :"))
+num2 = int(input("Enter the second number :"))
+
+
+print("+++ADDITION+++")
+res1 = num1 + num2
+print("The sum is ",res1)
+print("---SUBTRACTION---")
+res2 = num1 - num2
+print("The result is ",res2)
+print("***MULTIPLICATION***")
+res3 = num1 * num2
+print("The product is ", res3)
+print("///DIVISION///")
+res4 = num1/num2
+print("The result is ",res4)
+
+
+
+
+
+
+
+
